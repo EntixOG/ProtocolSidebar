f7b6cff006871ddf2b34d329f0ccd2446e307cdd
entixog <zittrich28@gmail.com>
2025-06-21 21:01:10 +0200

    feat: bump version to 6.2.11-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "me.catcoder"
-version = "6.2.10-SNAPSHOT"
+version = "6.2.11-SNAPSHOT"
 description = "Powerful feature-packed Minecraft scoreboard library"
 
 val adventureVersion = "4.16.0"
